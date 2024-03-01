06bd25a94fca44a3ebb8fa47e99341aacaf7ceb8
Lopez Nicolas <richtofen60@gmail.com>
2024-03-01 16:52:45 +0100

    add Outlet from router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Home from './screens/Home'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    
-    <Home />
+    <Outlet />
   )
 }
 
