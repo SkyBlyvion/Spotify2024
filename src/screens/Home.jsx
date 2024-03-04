8528c241272f8464758aaedc031d23b17591d81c
Lopez Nicolas <richtofen60@gmail.com>
2024-03-04 12:14:06 +0100

    add loader-spinner

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAlbums } from '../redux/album/albumSlice'
 import { selectAlbumsData } from '../redux/album/albumSelector'
+import PageLoader from '../components/loader/PageLoader'
 
 const Home = () => {
   // on récupère le hook useDispatch de react-redux
@@ -15,11 +16,16 @@ const Home = () => {
 
   // on recupére notre selector pour avoir accés aux données
   const { albums, loading } = useSelector(selectAlbumsData)
-  console.log('data albums', albums);
 
-  
+  // on peut specifier le tableau qui nous interesse
+  const dataAlbum = albums['hydra:member']
+  // console.log('data albums', dataAlbum);
+  console.log('data albums', dataAlbum);
 
   return (
+    loading ? <PageLoader/>:
+
+
     <div>Home</div>
   )
 }
