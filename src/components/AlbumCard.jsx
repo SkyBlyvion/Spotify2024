a5b1c6c16dc9ace7f59011badb218588e2de8ee1
Lopez Nicolas <richtofen60@gmail.com>
2024-03-07 16:53:39 +0100

    add albumsimg

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { albumUrl } from '../constants/apiConstant'
+import { Link } from 'react-router-dom'
 
 const AlbumCard = ({data}) => {
 
@@ -8,8 +9,14 @@ const AlbumCard = ({data}) => {
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white_01 hover:bg-white_05 transition-all ease-out duration-500 animate-slideup rounded-lg cursor-pointer'>
-        <div className='relative w-full h-56 flex-col'>
-            <img src={imgPath} alt={data} />
+        <div className='relative w-full flex-col'>
+            <Link to='/' state={{params: data}}>
+                <img className='card-sh rounded-lg object-cover' src={imgPath} alt={data.title} />
+            </Link>
+            <div className='mt-4 flex flex-col'>
+                <p className='text-white text-xl truncate font-bold'>{data.title}</p>
+                <p className='text-sm truncate text-white'>{data.artist.name}</p>
+            </div>
         </div>
     </div>
   )
