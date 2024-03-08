b41d8976480f30f01abb45221e94b85cd172d2ba
Lopez Nicolas <richtofen60@gmail.com>
2024-03-08 15:37:43 +0100

    add PlayerSlice

---
+++ src/redux/Player/PlayerSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+// on va init nos states dans une constante nommé initialState
+const initialState = {
+    currentSongs: [], // tableau de chansons
+    currentAlbum: [], // album en cours de lecture
+    currentIndex: 0, // index de la chanson en cours de lecture
+    isActive: false, // état du player
+    isPlaying: false, // etat de la lecture
+    activeSong: {}, // chanson en cours de lecture
+}
+
+// création du slice pour la gestion du player
+const playerSlice = createSlice({
+    name: 'player',
+    initialState,
+    reducers: {
+        // on va definir nos setters pour chaque propriété du state
+        // Tout ce que l'on stocke lorsceque l'on active une chanson
+        setActiveSong: (state, action) => {
+            // stockage de la chanson en cours de lecture dans ActiveSong
+            state.activeSong = action.payload?.songs[action.payload?.index];
+            // stockage du tableau de chansons
+            state.currentSongs = action.payload?.data?.songs;
+            // stockage de l'index de la chanson en cours de lecture
+            state.currentIndex = action.payload?.index;
+            // stockage de l'etat du player
+            state.isActive = true;
+        },
+
+        setActiveAlbum: (state, action) => {
+            // on stocke les infos de l'album
+            // ? si pas de data
+            state.currentAlbum = action.payload?.data;
+        },
+
+        // méthode pour avancer la lsite de lecture
+        nextSong: (state, action) => {
+            // on récupère la chanson dans le tableau a l'inde dnné
+            state.activeSong = state.currentSongs[action.payload];
+            // on stocke l'index
+            state.currentIndex = action.payload;
+            state.isActive = true;  
+        },
+
+        // méthode pour revenir à la chanson precedente
+        prevSong: (state, action) => {
+            // on recupère la chanson dans le tableau a l'index dnné
+            state.activeSong = state.currentSongs[action.payload];
+            // on stocke l'index
+            state.currentIndex = action.payload;
+            state.isActive = true;  
+        },
+
+        // on va changer l'etat du player
+        playPause: (state, action) => {
+            state.isPlaying = action.payload;
+        }
+
+    }
+})
+
+//export des actions
+export const {setActiveSong, setActiveAlbum, nextSong, prevSong, playPause} = playerSlice.actions
+
+//export du reducer
+export default playerSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import albumReducer from "./album/albumSlice"
+import playerReducer from "./Player/PlayerSlice"
 
 // Rayon de l'application (Store)
 const store = configureStore({
     // ajouter les reducers ici
     reducer: {
         albums: albumReducer,
-
-        
-
+        player: playerReducer,
     }
 })
 
