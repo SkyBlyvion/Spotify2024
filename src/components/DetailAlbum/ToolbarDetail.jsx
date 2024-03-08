d35998041da8c43298896420e23a4d8ca7f9b211
Lopez Nicolas <richtofen60@gmail.com>
2024-03-08 16:53:51 +0100

    Add ToolbarDetail

--- src/components/DetailAlbum/ToolbarDetail.jsx
+++ src/components/DetailAlbum/ToolbarDetail.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { playPause, setActiveAlbum, setActiveSong } from '../../redux/Player/PlayerSlice';
+import PageLoader from '../loader/PageLoader';
+import PlayPause from '../PlayPause';
 
-const ToolbarDetail = () => {
+const ToolbarDetail = ({dataAlbum}) => {
+
+  const songs = dataAlbum?.songs;
+  const [index, setIndex] = useState(0);
+  const dispatch = useDispatch();
+
+  // on recupére les données du slice 
+  const {isPlaying, activeSong} = useSelector(state => state.player)
+  const {loading} = useSelector(state => state.albums)
+
+  // on va definir la methode pause
+  const handlePauseClick = () => {
+    dispatch(playPause(false))
+  } 
+
+  // méthode pour lancer la lecture
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({songs, data, index}));
+    dispatch(setActiveAlbum({data}));
+    dispatch(playPause(true));
+  }
+  
   return (
-    <div>ToolbarDetail</div>
+    loading ? <PageLoader /> :
+    <div className='cursor-pointer mr-3 '>
+      <PlayPause
+        songs={songs}
+        handlePause={handlePauseClick}
+        handlePlay={()=>handlePlayClick(index)}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        index={index}
+        data={dataAlbum}
+      />
+    </div>
   )
 }
 
